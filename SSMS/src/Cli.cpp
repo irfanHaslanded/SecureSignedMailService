256e8eda03194fdab8ef313d7122c1787a4353be
ezqurna <naief.qureshi@ericsson.com>
2020-03-06 13:48:20 +0100

    Changes in cli.h and cli.cpp

---
+++ SSMS/src/Cli.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+namespace ssms {
+void Cli::start(void) {
+}
+
+Cli::Cli() {
+}
+
+Cli::~Cli() {
+}
+
+void Cli::processIo() {
+}
+
+void Cli::printHelpText() {
+}
+
+std::string Cli::getPrompt() {
+}
+
+bool Cli::createUser(User& user) {
+}
+
+bool Cli::deleteUser(User& user) {
+}
+
+bool Cli::logIn(std::string username, std::string password) {
+}
+}
---
+++ SSMS/src/Cli.h
@@ -0,0 +1,23 @@
+#ifndef SSMS_CLI_H_
+#define SSMS_CLI_H_
+namespace ssms{
+class Cli
+{
+  public:
+    static void start(void);
+
+  private:
+    Cli();
+    virtual ~Cli();
+    void processIo();
+    void printHelpText();
+    std::string getPrompt();
+    bool createUser(User &user);
+    bool deleteUser(User &user);
+    bool logIn(std::string username, std::string password);
+    std::list<User> users;
+    User &loggedInUser;
+};
+}
+#endif
+
