a6d306f0aafc1ee9ac89daa5e8c5f56c5210e2ab
ezdezta <tamas.dezso@ericsson.com>
2020-03-09 21:05:20 +0100

    Add abstracts to MailBox class

--- SSMS/src/MailBox.h
+++ SSMS/src/MailBox.h
@@ -8,13 +8,39 @@
 
 namespace ssms {
 
+/**
+ * MailBox class. Each user has a MailBox, via which
+ * it is able to send encrypted messages into other
+ * Users' mailboxes, and into which it is able to get
+ * encrypted messages from other users.
+ */
 class MailBox {
 
+  /**
+   * A message consists of two parts
+   * @param first The name of the sender user
+   * @param second The body of the message
+   */
 using Msg = std::pair<std::string, std::string>;
 
 public:
+  /**
+   * Constructor
+   * @param owner User to whom the mailbox belongs
+   */
   MailBox(const User& owner);
+
+  /**
+   * Send encrypted message
+   * @param to Recipient user's inbox
+   * @param plain_msg Text of the message
+   */
   void sendMsg(MailBox& to, const std::string& plain_msg);
+
+  /**
+   * Get all the received messages
+   * @returns a list of decrypted messages
+   */
   std::list<Msg> getReceivedMsgs() const;
 
 private:
