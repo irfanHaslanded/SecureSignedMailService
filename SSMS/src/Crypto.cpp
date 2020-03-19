a80ffe979c664720f882d37583b983c4c4507e48
EVEGOPI <venkata.gopi.krishna@ericsson.com>
2020-03-19 15:46:46 +0100

    Revert "Encrypt a message using EVP functions."
    This reverts commit fcb574c031c6b00024b0f430b33cc10031ba6104.

--- SSMS/src/Crypto.cpp
+++ SSMS/src/Crypto.cpp
@@ -13,29 +13,12 @@
 namespace ssms {
 const int saltSize = 16;
 const int saltIdSize = 3;  // eaxmple "$6$"
-
-EVP_PKEY* Crypto::localKeypair;
-EVP_PKEY* Crypto::remotePublicKey;
-EVP_CIPHER_CTX* Crypto::rsaEncryptContext;
-EVP_CIPHER_CTX* Crypto::rsaDecryptContext;
 static const std::string hashValidChars {
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./"};
 
 std::string Crypto::encrypt(const std::string &message,
                             const std::string &publickey) {
-
-  // Encrypt the message with RSA
-  // +1 on the string length argument because we want to encrypt the NUL terminator too
-  unsigned char *encryptedMessage = NULL;
-  unsigned char *iv;
-  size_t publickeyLength = sizeof(publickey);
-  size_t ivLength;
-
-  int encryptedMessageLength = rsaEncrypt((const unsigned char*)message.c_str(), message.size()+1,
-    &encryptedMessage, (unsigned char**)publickey.c_str(), &publickeyLength, &iv, &ivLength);
-
-   std::string s((char*)encryptedMessage);
-  return s;
+  return message;
 }
 
 std::string Crypto::decrypt(const std::string &message,
@@ -43,8 +26,8 @@ std::string Crypto::decrypt(const std::string &message,
   return message;
 }
 
-bool Crypto::validatePassword(const std::string &hash, const std::string &password)
-{
+bool Crypto::validatePassword(const std::string &hash,
+                              const std::string &password) {
   return hash == genHash(Crypto::getSalt(hash), password);
 
 }
@@ -52,7 +35,7 @@ bool Crypto::validatePassword(const std::string &hash, const std::string &passwo
 std::string Crypto::genPassword(const std::string &password) {
 
   std::string salt = passTheSalt();
-
+  std::cout << salt << std::endl; // for UT debugging purposes
   return genHash(salt, password);
 
 }
@@ -88,134 +71,4 @@ std::string Crypto::getSalt(const std::string &hash) {
   std::cout << "salt is  : " + salt << std::endl; // for UT debugging purposes
   return salt;
 }
-
-int Crypto::init() {
-
-  //Initialize keys
-  localKeypair = NULL;
-  remotePublicKey = NULL;
-
-  // Initalize contexts
-  rsaEncryptContext = EVP_CIPHER_CTX_new();
-
-  rsaDecryptContext = EVP_CIPHER_CTX_new();
-
-
-  // Check if any of the contexts initializations failed
-  if(rsaEncryptContext == NULL || rsaDecryptContext == NULL) {
-    return FAILURE;
-  }
-
-  //IV -Initilization vector can be left to null for rsa cipher
-
-  // Generate RSA keys
-  generateRsaKeypair(&localKeypair);
-  // Generate public key
-  generateRsaKeypair(&remotePublicKey);
-  //TODO : setRemotePublicKey(remotePublicKey, remotePublicKeyLength);
-
-  return SUCCESS;
-}
-
-void Crypto::free(){
-
-  EVP_PKEY_free(remotePublicKey);
-
-  EVP_CIPHER_CTX_free(rsaEncryptContext);
-
-  EVP_CIPHER_CTX_free(rsaDecryptContext);
-}
-
-int Crypto::generateRsaKeypair(EVP_PKEY **keypair) {
-  EVP_PKEY_CTX *context = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
-
-  if(EVP_PKEY_keygen_init(context) <= 0) {
-    return FAILURE;
-  }
-
-  if(EVP_PKEY_CTX_set_rsa_keygen_bits(context, RSA_KEYLEN) <= 0) {
-    return FAILURE;
-  }
-
-  if(EVP_PKEY_keygen(context, keypair) <= 0) {
-    return FAILURE;
-  }
-
-  EVP_PKEY_CTX_free(context);
-
-  return SUCCESS;
-}
-
-int Crypto::rsaEncrypt(const unsigned char *message, size_t messageLength, unsigned char **encryptedMessage, unsigned char **encryptedKey,
-  size_t *encryptedKeyLength, unsigned char **iv, size_t *ivLength) {
-
-  // Allocate memory for everything
-  size_t encryptedMessageLength = 0;
-  size_t blockLength = 0;
-
-  *encryptedKey = (unsigned char*)malloc(EVP_PKEY_size(remotePublicKey));
-  *iv = (unsigned char*)malloc(EVP_MAX_IV_LENGTH);/*Make sure to add more info about IV*/
-  *ivLength = EVP_MAX_IV_LENGTH;
-
-  if(*encryptedKey == NULL || *iv == NULL) {
-    return FAILURE;
-  }
-
-  *encryptedMessage = (unsigned char*)malloc(messageLength + EVP_MAX_IV_LENGTH);
-  if(encryptedMessage == NULL) {
-    return FAILURE;
-  }
-
-  // Encrypt it!
-  /*Intializes a cipher context "rsaEncryptContext" for encryption with symmetric cipher type "EVP_aes_256_cbc()" using random secret key and IV. This secret key is encrypted using a public key.
-  We can also encrypt with multiple pub keys*/
-  /*EVP_aes_256_cbc() is the AES-256 cipher type. AES - Advanced encryption standard*/
-  if(!EVP_SealInit(rsaEncryptContext, EVP_aes_256_cbc(), encryptedKey, (int*)encryptedKeyLength, *iv, &remotePublicKey, 1)) {
-    return FAILURE;
-  }
-
-  /*encrypts "messageLength" bytes from the "message" in and writes the encrypted version to "*encryptedMessage + encryptedMessageLength" of size "(int*)&blockLength"*/
-  /*In our case we are encrypting only a single message, we can always call this multiple times and do line by line seperate encryption if needed.*/
-  /*The update function returns the number of bytes that are encrypted.*/
-  if(!EVP_SealUpdate(rsaEncryptContext, *encryptedMessage + encryptedMessageLength, (int*)&blockLength, (const unsigned char*)message, (int)messageLength)) {
-    return FAILURE;
-  }
-  encryptedMessageLength += blockLength;
-
-  /*Finalizing encryption*/
-  /*This will pad extra space in buffer to fill up last block.*/
-  if(!EVP_SealFinal(rsaEncryptContext, *encryptedMessage + encryptedMessageLength, (int*)&blockLength)) {
-    return FAILURE;
-  }
-  encryptedMessageLength += blockLength;
-
-  /*Returning the number of bytes encrypted. It is a binary string.*/
-  return (int)encryptedMessageLength;
-}
-
-int Crypto::getLocalPublicKey(unsigned char **publicKey) {
-   //TODO
-  int key;
-  return key;
-}
-
-int Crypto::getLocalPrivateKey(unsigned char **privateKey) {
-  //TODO
-  int key;
-  return key;
-}
-
-int Crypto::getRemotePublicKey(unsigned char **publicKey) {
-  //TODO
-  int key;
-  return key;
-}
-
-int Crypto::setRemotePublicKey(unsigned char *publicKey, size_t publicKeyLength) {
-  //TODO
-  int key;
-  return key;
-}
-
-
 } 
--- SSMS/src/Crypto.h
+++ SSMS/src/Crypto.h
@@ -10,20 +10,8 @@
 
 #include <cstring>
 #include <iostream>
-#include <openssl/evp.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
 #include <openssl/rand.h>
 #include <string>
-
-#define RSA_KEYLEN 2048
-
-#define SUCCESS 0
-#define FAILURE -1
-
-#define KEY_SERVER_PRI 0
-#define KEY_SERVER_PUB 1
-#define KEY_CLIENT_PUB 2
 
 namespace ssms {
 class Crypto
@@ -36,31 +24,6 @@ public:
   static std::string passTheSalt();
   static std::string getSalt(const std::string &hash);
   static std::string genPassword(const std::string &password);
-
-
-  /*Functions for rsa encryption and decryption*/
-  static int rsaEncrypt(const unsigned char *message, size_t messageLength, unsigned char **encryptedMessage, unsigned char **encryptedKey,
-      size_t *encryptedKeyLength, unsigned char **iv, size_t *ivLength);
-
-  int getLocalPublicKey(unsigned char **publicKey);
-  int getLocalPrivateKey(unsigned char **privateKey);
-
-  int getRemotePublicKey(unsigned char **publicKey);
-  int setRemotePublicKey(unsigned char *publicKey, size_t publicKeyLength);
-
-private:
-  static EVP_PKEY *localKeypair;
-  static EVP_PKEY *remotePublicKey;
-
-  static EVP_CIPHER_CTX *rsaEncryptContext;
-  static EVP_CIPHER_CTX *rsaDecryptContext;
-
-  static int init();
-  void free();
-  static int generateRsaKeypair(EVP_PKEY **keypair);
-
-
-
 };
 }; // namespace ssms
 
